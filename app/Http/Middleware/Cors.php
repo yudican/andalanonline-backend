6614af5c0063052f8321ea73de51cff410eeff6d
Yudi Candra <yudhichandra02@gmail.com>
2022-07-21 19:54:37 +0700

    update cors

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -16,11 +16,6 @@ class Cors
      */
     public function handle(Request $request, Closure $next)
     {
-        return $next($request)
-            ->header('Access-Control-Allow-Origin', '*')
-            ->header('Access-Control-Allow-Methods', '*')
-            ->header('Access-Control-Allow-Credentials', true)
-            ->header('Access-Control-Allow-Headers', 'X-Requested-With,Content-Type,X-Token-Auth,Authorization')
-            ->header('Accept', 'application/json');
+        return $next($request);
     }
 }
